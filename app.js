fe1da127684d7dd85b4c47b0df75ba29a1c10a0b
stanley zheng <dev.stanzheng@gmail.com>
2014-07-24 10:36:13 -0400

    adding a public directory

--- app.js
+++ app.js
@@ -2,12 +2,20 @@ var express = require('express'),
   app = express(),
   path = require('path'),
   http = require('http'),
+  bodyParser = require('body-parser'),
   Client = require('xtuple-rest-client');
 
 app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'ejs');
 app.locals.title = "xTuple REST To Do App";
 app.set("views", path.join( __dirname, "views" ));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+//remove warning
 
 app.get('/', function (req, res){
   new Client(function (client) {
